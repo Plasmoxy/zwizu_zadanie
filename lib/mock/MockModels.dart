0bf254e538007683c21c93aa69316945c38ed8c8
Sebastián Petrík <plasmoxy@gmail.com>
2021-07-13 20:42:25 +0200

    Update MockModels.dart

--- lib/mock/MockModels.dart
+++ lib/mock/MockModels.dart
@@ -6,7 +6,7 @@ import 'package:zwizu_zadanie/models/VideoEntry.dart';
 class MockModels {
   static final MOCK_TOURNAMENT = Tournament(
     name: "Turnaj 22",
-    date: DateTime(2022, 09, 07, 19),
+    date: DateTime(2022, 09, 07, 20),
   );
 
   static final MOCK_VIDEO_ENTRIES = <VideoEntry>[
